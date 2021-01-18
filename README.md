6a912f2b5229dda7b7b933799e800558744cc307
liangyong <475314405@qq.com>
2021-01-18 10:38:32 +0800

    bug

--- README.md
+++
@@ -1,9 +0,0 @@
-```bash
-npm i 安装依赖
-
-开发：npm start 默认端口号为8080
-
-预览包：npm run preview
-生产包：npm run build
-```
-
--- src/pages/index/app.js
+++
@@ -1,9 +0,0 @@
-import layout from './index.vue';
-
-new Vue({
-    el: '#app',
-    components: {
-        layout
-    },
-    template: '<layout/>'
-})
