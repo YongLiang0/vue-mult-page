d3d44e105ebc100708e53a4e079c8f859eb0c906
liangyong <475314405@qq.com>
2021-01-18 17:55:32 +0800

    test

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,25 @@
 <template>
     <div class="page-index">
-        
+        <!-- <mini-player :video="video"></mini-player> -->
     </div>
 </template>
 
 <script>
 import mixin from '@/mixin/index.js';
 import INTERFACE from '@/http/interface/interface_module1.js';
+import miniPlayer from '@/components/miniPlayer/index.vue';
 export default {
     mixins : [ mixin ],
+
+    components : {
+        miniPlayer
+    },
+
     data(){
         return{
-            
+            video : {
+                url : '/static/file/blackpink.mp4'
+            }
         }
     },
 
@@ -26,7 +34,9 @@ export default {
          */
         async test(){
             let res = await INTERFACE.getList1();
-            console.log(res)
+            if(res){
+
+            }
         }
     }
 }
